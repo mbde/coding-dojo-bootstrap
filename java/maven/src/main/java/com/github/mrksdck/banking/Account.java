4aab1790afeebc2988aeeaa5643037144d9c1669
Markus Decke <mbde@users.noreply.github.com>
2016-04-24 19:39:59 +0200

    Adding implementation for Account
    * Test commited in earlier commit

--- java/maven/src/main/java/com/github/mrksdck/banking/Account.java
+++ java/maven/src/main/java/com/github/mrksdck/banking/Account.java
@@ -1,5 +1,8 @@
 package com.github.mrksdck.banking;
 
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,12 +11,28 @@ import java.util.List;
  */
 public class Account {
     private List<Statement> history;
+    private Money balance;
 
     public Account() {
         history = new ArrayList<>();
+        balance = Money.of(CurrencyUnit.EUR, 0.0);
     }
 
     public List<Statement> getHistory() {
         return history;
+    }
+
+    public void process(Deposit deposit) {
+        balance = balance.plus(deposit.getAmount());
+        history.add(new Statement(deposit, balance));
+    }
+
+    public void process(Withdrawal withdrawal) {
+        balance = balance.minus(withdrawal.getAmount());
+        history.add(new Statement(withdrawal, balance));
+    }
+
+    public Money getBalance() {
+        return balance;
     }
 }
